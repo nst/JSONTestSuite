309149f6a6b9bea33e88859211552da276ef54b2
Cédric Luthi <cedric.luthi@gmail.com>
2021-02-27 01:12:14 +0100

    Update the Newtonsoft.Json parser to version 12.0.3
    Also update the project to use the new `setup` command and .NET 5 tooling. Installing the [.NET 5 SDK](https://dotnet.microsoft.com/download/dotnet/5.0) is required for this parser to work.

---
+++ parsers/test_dotnet_newtonsoft/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+if (args.Length != 1)
+{
+    Console.Error.WriteLine($"Usage: {Path.GetFileName(Environment.GetCommandLineArgs()[0])} file.json");
+    return 1;
+}
+
+try
+{
+    using var fileStream = new FileStream(args[0], FileMode.Open);
+    using var streamReader = new StreamReader(fileStream);
+    using var jsonReader = new JsonTextReader(streamReader);
+    try
+    {
+        var serializer = JsonSerializer.Create(new JsonSerializerSettings { MaxDepth = 512 });
+        var result = serializer.Deserialize(jsonReader);
+        Console.WriteLine(result);
+        return 0;
+    }
+    catch (Exception e)
+    {
+        Console.Error.WriteLine(e);
+        return 1;
+    }
+}
+catch (Exception e)
+{
+    Console.Error.WriteLine(e);
+    return 2;
+}
